4ac11cb5b36daf6db013596c7d4cc23c7df9e683
hs-gautam <gautam@genero.jp>
2025-08-25 17:37:29 +0530

    Added block plugin.

---
+++ mymodule/src/Plugin/Block/WeatherApiBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\mymodule\Plugin\Block;
+
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Provides a weather api block.
+ */
+#[Block(
+  id: 'mymodule_weather_api',
+  admin_label: new TranslatableMarkup('Weather API'),
+  category: new TranslatableMarkup('QR'),
+)]
+final class WeatherApiBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build['content'] = [
+      '#markup' => $this->t('Current weather is 25 degree.'),
+    ];
+    return $build;
+  }
+
+}
